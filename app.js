1fd3a7e066d756663d2c9214d5736844827a3477
Wahaj khan <154734596+wahaj906@users.noreply.github.com>
2024-03-07 11:09:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,75 @@
+
+
+let myName = "Wahaj"
+
+let Age = "20"
+
+let Passion = "Programming"
+
+let = Hobby = "Coding & Movie"
+
+console.log(`Name:`, myName);
+
+console.log("Age:", Age);
+
+console.log("Passion:", Passion);
+
+console.log(`Hobby:`, Hobby);
+
+//
+
+/*let MySelf = {
+    Name: "WAHAJ",
+    Age: "20",
+    Qualification: "Intermediate in CS",
+    Hobby: "Coding & Movie",
+}*/
+
+//
+
+//       Table of 17         //
+
+console.log("17 x 1 =", 17 * 1);
+
+console.log("17 x 2 =", 17 * 2);
+
+console.log("17 x 3 =", 17 * 3);
+
+console.log("17 x 4 =", 17 * 4);
+
+console.log("17 x 5 =", 17 * 5);
+
+console.log("17 x 6 =", 17 * 6);
+
+console.log("17 x 7 =", 17 * 7);
+
+console.log("17 x 8 =", 17 * 8);
+
+console.log("17 x 9 =", 17 * 9);
+
+console.log("17 x 10 =", 17 * 10);
+
+//
+
+let a = 55;
+
+let b = 20;
+
+// a or b value
+
+console.log("a = 4 b = 9");
+
+console.log("a x b =", a * b);
+
+console.log("++b =", ++b);
+
+console.log("a - b =", a - b);
+
+console.log("a / b =", a / b);
+
+console.log("17", -2 * 2 + 9);
+
+console.log("a = b", -2 * 4 / 2);
+
+console.log("a = b", 2 + 9 * 9);
+
